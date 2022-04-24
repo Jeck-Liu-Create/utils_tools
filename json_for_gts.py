c835e75d711a904329c3ffc69880bec200a02fd7
han-xilin <985063681@qq.com>
2022-04-24 14:10:53 +0800

    Initial commit

---
+++ json_for_gts.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   json_for_gts.py    
+@Contact :  tclyldx@163.com
+@License :   (C)Copyright LiuDongxing,All Rights Reserved. 
+             Unauthorized copying of this file, via any medium is strictly prohibited 
+             Proprietary and confidential
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2022/4/24 14:07   LiuDongxing      1.0   Convert JSON files to GTS
+'''
+
+# import lib
